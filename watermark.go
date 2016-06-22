2b590ee04fd692a9b5ab54c8f3f3697442b8e0a2
Collin Guarino <collin.guarino@gmail.com>
2016-06-21 20:24:57 -0700

    Working base implementation.

--- watermark.go
+++ watermark.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"image"
 	"image/draw"
 	"image/jpeg"
@@ -13,8 +12,8 @@ import (
 )
 
 func main() {
+	// Parse arguments
 	args := os.Args[1:]
-
 	var files []string
 	if len(args) == 0 {
 		log.Print("Enter a picture source, '*' or '.' for all")
@@ -22,45 +21,24 @@ func main() {
 	}
 	if strings.EqualFold(args[0], ".") || strings.EqualFold(args[0], "*") {
 		// Get all files in current directory
-		files, err := ioutil.ReadDir(".")
-		for _, f := range files {
-			files = append(files, f)
+		fileList, err := ioutil.ReadDir(".")
+		for _, f := range fileList {
+			// fmt.Println(f.Name())
+			files = append(files, f.Name())
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
 		files = []string{args[0]}
-	}
-	watermark(files)
-}
-
-func watermark(files []string) {
-	for _, file := range files {
-		originalBytes, _ := os.Open(file)
-		originalImage, _ := jpeg.Decode(originalBytes)
-		defer originalBytes.Close()
-
-		// _ = original
-		watermarkBytes, _ := os.Open("watermark.jpeg")
-		watermark, _ := png.Decode(watermarkBytes)
-		defer watermarkBytes.Close()
-
-		offsetWidth := 200
-		offsetHeight := 200
-		offset := image.Pt(offsetWidth, offsetHeight)
-		bounds := originalImage.Bounds()
-		colorRange := image.NewRGBA(bounds)
-		draw.Draw(colorRange, bounds, originalImage, image.ZP, draw.Src)
-		draw.Draw(colorRange, watermark.Bounds().Add(offset), watermark, image.ZP, draw.Over)
-
-		newImageName := strings.SplitAfter(file, ".")
-		_ = newImageName
-		// newImage, _ := os.Create(file + "_watermark"
+		log.Print("Adding watermark to image " + files[0])
 	}
 
+	watermark := Watermark{Position: TOP_RIGHT}
+	watermark.apply(files[0])
 }
 
+// Watermark quadrant position
 const (
 	CENTER       = 0
 	TOP_LEFT     = 1
@@ -78,4 +56,42 @@ type Watermark struct {
 
 	Position int    // Placement of watermark image
 	Source   string // File path of image
+}
+
+func (w *Watermark) apply(file string) error {
+	originalBytes, _ := os.Open(file)
+	originalImage, _ := jpeg.Decode(originalBytes)
+	originalImageConfig, _, err := image.DecodeConfig(originalBytes)
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Error getting original image's configuration.")
+	}
+	bounds := originalImage.Bounds()
+	defer originalBytes.Close()
+
+	watermarkBytes, err := os.Open("watermark.png")
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Error opening watermark image.")
+	}
+	watermark, err := png.Decode(watermarkBytes)
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Error decoding watermark.")
+	}
+	defer watermarkBytes.Close()
+
+	offset := image.Pt(originalImageConfig.Width, originalImageConfig.Height)
+	colorRange := image.NewRGBA(bounds)
+	draw.Draw(colorRange, bounds, originalImage, image.ZP, draw.Src)
+	draw.Draw(colorRange, watermark.Bounds().Add(offset), watermark, image.ZP, draw.Over)
+
+	baseName := strings.Split(file, ".")[0]
+	extension := "." + strings.Split(file, ".")[1]
+	newImage, err := os.Create(baseName + "_watermark" + extension)
+	jpeg.Encode(newImage, colorRange, &jpeg.Options{jpeg.DefaultQuality})
+	defer newImage.Close()
+	log.Print("Finished")
+
+	return nil
 }
